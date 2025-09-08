c1adaaac85f72f4d7df7e619dd0f3bd7e3f34dd2
MonadWizard <monad.wizard.r@gmail.com>
2025-09-08 14:54:34 +0600

    create add directories as template for roadmap

---
+++ create_all_directory.sh
@@ -0,0 +1,23 @@
+while read -r d; do
+  mkdir -p "$d"/{concept,code}
+done <<'EOF'
+Step1_Learn the basics
+Step2_Learn Important Sorting Techniques
+Step3_Solve Problems on Arrays
+Step4_Binary Search
+Step5_Strings
+Step6_Learn LinkedList
+Step7_Recursion
+Step8_Bit Manipulation
+Step9_Stack and Queues
+Step10_Sliding Window & Two Pointer Combined Problems
+Step11_Heaps
+Step12_Greedy Algorithms
+Step13_Binary Trees
+Step14_Binary Search Trees
+Step15_Graphs
+Step16_Dynamic Programming
+Step17_Tries
+Step18_Strings
+EOF
+
